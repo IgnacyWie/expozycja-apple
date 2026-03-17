4bfdfd6c9627f0700ec7672a89c45d4294b1cf5e
IgnacyWie <ignacywie@icloud.com>
2026-03-17 17:18:31 +0100

    style(logo): changed logo

--- components/blocks/hero-section-1.tsx
+++ components/blocks/hero-section-1.tsx
@@ -721,7 +721,7 @@ function FooterSection() {
 const Logo = ({ className }: { className?: string }) => {
   return (
     <span className={cn('text-lg font-semibold tracking-tight', className)}>
-      <img className="h-8"
+      <img className="h-6"
         src="/logo.png"
       />
     </span>
