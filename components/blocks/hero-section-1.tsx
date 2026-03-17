081fab7dffd3f116b8d5b649ac7c2a58907e0ef8
IgnacyWie <ignacywie@icloud.com>
2026-03-17 12:53:18 +0100

    style(background): removed the redundant background

--- components/blocks/hero-section-1.tsx
+++ components/blocks/hero-section-1.tsx
@@ -66,40 +66,6 @@ export function HeroSection() {
         </div>
         <section id="home" className="scroll-mt-24">
           <div className="relative pt-24 md:pt-36">
-            <AnimatedGroup
-              variants={{
-                container: {
-                  visible: {
-                    transition: {
-                      delayChildren: 1,
-                    },
-                  },
-                },
-                item: {
-                  hidden: {
-                    opacity: 0,
-                    y: 20,
-                  },
-                  visible: {
-                    opacity: 1,
-                    y: 0,
-                    transition: {
-                      type: 'spring',
-                      bounce: 0.3,
-                      duration: 2,
-                    },
-                  },
-                },
-              }}
-              className="absolute inset-0 -z-20">
-              <img
-                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?w=3276&h=4095&fit=crop&q=75"
-                alt="background"
-                className="absolute inset-x-0 top-56 -z-20 hidden lg:top-32 dark:block"
-                width="3276"
-                height="4095"
-              />
-            </AnimatedGroup>
             <div aria-hidden className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--background)_75%)]" />
             <div className="mx-auto max-w-7xl px-6">
               <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
